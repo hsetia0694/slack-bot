558a57d47a7853e689a8c0a7a654543967a82a54
hsetia0694 <38557678+hsetia0694@users.noreply.github.com>
2021-07-11 14:02:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const request = require('request-promise');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const WEBHOOK = 'https://hooks.slack.com/services/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';
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.post('/slack', (req, res) => {
+    console.log('Request received at API level');
+
+    const body = req.body;
+    body['mkdwn'] = true;
+    const response = request({
+        method: 'POST',
+        body: body,
+        url: WEBHOOK,
+        json: true
+    })
+
+    console.log("Sent message to slack");
+    res.send("Sent message to slack");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+})
+
+
+// (async function () {
+//     try {
+//         // GET data
+//         // const data = await getData();
+//         // console.log('DATA ',data)
+
+//         //POST to slack
+//         const body = {
+//             mkdwn: true,
+//             text: 'This is really great slack message',
+//             attachments: data.map(person => ({
+//                 color: 'good',
+//                 text: `This email address - *${person.email}* belongs to *${person.name}*`
+//             }))
+//         }
+//         console.log('BODY ', body)
+//         const res = await request({
+//             method: 'POST',
+//             body: body,
+//             url: WEBHOOK,
+//             json: true
+//         })
+//         console.log('Response ', res)
+//     } catch (e) {
+//         console.log('Error ', e);
+//     }
+// })();
